408939367e71d0b92dd7109d96bb2ca745210ab6
Matti Astrand <mattiastr@gmail.com>
2025-07-05 21:53:35 -0400

    Add flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A simple package";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixos-24.11";
+
+  outputs = { self, nixpkgs }:
+    let
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+      devShell = forAllSystems (system:
+        let pkgs = nixpkgsFor.${system};
+        in pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+            # goimports
+            go-tools
+          ];
+        });
+    };
+}
