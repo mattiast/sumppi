f38006eb6196343cf15f7eb29f13ca790430b604
Matti Astrand <mattiastr@gmail.com>
2025-07-05 23:25:13 -0400

    Include itunes tags

--- main.go
+++ main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
-
-	"github.com/gorilla/feeds"
 )
 
 type SeriesConfig struct {
@@ -122,20 +121,77 @@ func fetchSeriesData(guid string) (*SeriesData, error) {
 	return &apiResponse.Data, nil
 }
 
-func generateRSSFeed(seriesData *SeriesData) (*feeds.Feed, error) {
+type RSSFeed struct {
+	XMLName xml.Name `xml:"rss"`
+	Version string   `xml:"version,attr"`
+	Xmlns   string   `xml:"xmlns:itunes,attr"`
+	Channel Channel  `xml:"channel"`
+}
+
+type Channel struct {
+	Title         string `xml:"title"`
+	Link          string `xml:"link"`
+	Description   string `xml:"description"`
+	Language      string `xml:"language"`
+	Copyright     string `xml:"copyright"`
+	PubDate       string `xml:"pubDate"`
+	LastBuildDate string `xml:"lastBuildDate"`
+	ITunesAuthor  string `xml:"itunes:author"`
+	ITunesImage   Image  `xml:"itunes:image"`
+	Items         []Item `xml:"item"`
+}
+
+type Image struct {
+	Href string `xml:"href,attr"`
+}
+
+type Item struct {
+	Title       string    `xml:"title"`
+	Description string    `xml:"description"`
+	PubDate     string    `xml:"pubDate"`
+	GUID        string    `xml:"guid"`
+	Enclosure   Enclosure `xml:"enclosure"`
+	ITunesAuthor string   `xml:"itunes:author"`
+	ITunesDuration string `xml:"itunes:duration"`
+}
+
+type Enclosure struct {
+	URL    string `xml:"url,attr"`
+	Length string `xml:"length,attr"`
+	Type   string `xml:"type,attr"`
+}
+
+func formatDuration(seconds int) string {
+	hours := seconds / 3600
+	minutes := (seconds % 3600) / 60
+	secs := seconds % 60
+	
+	if hours > 0 {
+		return fmt.Sprintf("%d:%02d:%02d", hours, minutes, secs)
+	}
+	return fmt.Sprintf("%d:%02d", minutes, secs)
+}
+
+func generateRSSFeed(seriesData *SeriesData) (string, error) {
 	pubDate, err := time.Parse(time.RFC3339, seriesData.PublicationDate)
 	if err != nil {
 		pubDate = time.Now()
 	}
 
-	feed := &feeds.Feed{
-		Title:       seriesData.Title,
-		Link:        &feeds.Link{Href: seriesData.Link},
-		Description: seriesData.Description,
-		Author:      &feeds.Author{Name: seriesData.Author},
-		Created:     pubDate,
-		Copyright:   seriesData.Copyright,
-		Image:       &feeds.Image{Url: seriesData.CoverURL},
+	feed := RSSFeed{
+		Version: "2.0",
+		Xmlns:   "http://www.itunes.com/dtds/podcast-1.0.dtd",
+		Channel: Channel{
+			Title:         seriesData.Title,
+			Link:          seriesData.Link,
+			Description:   seriesData.Description,
+			Language:      "fi",
+			Copyright:     seriesData.Copyright,
+			PubDate:       pubDate.Format(time.RFC1123Z),
+			LastBuildDate: time.Now().Format(time.RFC1123Z),
+			ITunesAuthor:  seriesData.Author,
+			ITunesImage:   Image{Href: seriesData.CoverURL},
+		},
 	}
 
 	for _, episode := range seriesData.Episodes {
@@ -144,24 +200,29 @@ func generateRSSFeed(seriesData *SeriesData) (*feeds.Feed, error) {
 			episodePubDate = time.Now()
 		}
 
-		item := &feeds.Item{
+		item := Item{
 			Title:       episode.Title,
-			Link:        &feeds.Link{Href: episode.AudioURL},
 			Description: episode.Description,
-			Author:      &feeds.Author{Name: episode.Author},
-			Created:     episodePubDate,
-			Id:          episode.GUID,
-			Enclosure: &feeds.Enclosure{
-				Url:    episode.AudioURL,
-				Type:   "audio/mpeg",
+			PubDate:     episodePubDate.Format(time.RFC1123Z),
+			GUID:        episode.GUID,
+			Enclosure: Enclosure{
+				URL:    episode.AudioURL,
 				Length: fmt.Sprintf("%d", episode.AudioLength),
+				Type:   "audio/mpeg",
 			},
+			ITunesAuthor:   episode.Author,
+			ITunesDuration: formatDuration(episode.AudioDuration),
 		}
 
-		feed.Items = append(feed.Items, item)
+		feed.Channel.Items = append(feed.Channel.Items, item)
 	}
 
-	return feed, nil
+	xmlData, err := xml.MarshalIndent(feed, "", "  ")
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal XML: %w", err)
+	}
+
+	return xml.Header + string(xmlData), nil
 }
 
 func main() {
@@ -181,14 +242,9 @@ func main() {
 	fmt.Printf("Series: %s by %s\n", seriesData.Title, seriesData.Author)
 	fmt.Printf("Episodes: %d\n", len(seriesData.Episodes))
 
-	feed, err := generateRSSFeed(seriesData)
+	rssXML, err := generateRSSFeed(seriesData)
 	if err != nil {
 		log.Fatalf("Error generating RSS feed: %v", err)
-	}
-
-	rssXML, err := feed.ToRss()
-	if err != nil {
-		log.Fatalf("Error converting to RSS XML: %v", err)
 	}
 
 	filename := fmt.Sprintf("%s.rss", guid)
